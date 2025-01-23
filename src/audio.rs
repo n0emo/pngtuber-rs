1442aaa5ce204c800a811548050766f183e67f4a
Albert Shefner <solaris945@gmail.com>
2025-01-23 04:08:12 +0300

    Implement settings page

--- src/audio.rs
+++ src/audio.rs
@@ -1,11 +1,100 @@
 use std::sync::{Arc, RwLock};
 
 use cpal::{
-    traits::{DeviceTrait as _, HostTrait as _},
-    Stream,
+    traits::{DeviceTrait as _, HostTrait as _, StreamTrait as _},
+    BuildStreamError, DefaultStreamConfigError, Device, DeviceNameError, DevicesError, Host,
+    PlayStreamError, Stream,
 };
+use thiserror::Error;
 
 const GAIN_MINUS_INFINITY: f32 = 0.00001;
+
+#[derive(Debug, Error)]
+pub enum AudioError {
+    #[error("There is no audio devices in the system")]
+    NoDevice,
+
+    #[error("Could not get default stream config: {0}")]
+    DefaultStreamConfig(#[from] DefaultStreamConfigError),
+
+    #[error("Could not build audio stream: {0}")]
+    BuildStream(#[from] BuildStreamError),
+
+    #[error("Could not list input devices: {0}")]
+    Devices(#[from] DevicesError),
+
+    #[error("Could not get device name: {0}")]
+    DeviceName(#[from] DeviceNameError),
+
+    #[error("Could not start audio stream: {0}")]
+    PlayStream(#[from] PlayStreamError),
+}
+
+#[allow(unused)]
+pub struct AudioInterface {
+    buf: Arc<RwLock<AudioBuffer>>,
+    stream: Stream,
+    device: Device,
+    host: Host,
+}
+
+impl AudioInterface {
+    pub fn new(name: Option<&str>) -> Result<Self, AudioError> {
+        let buf = Arc::<RwLock<AudioBuffer>>::default();
+        let buf_s = buf.clone();
+
+        let host = cpal::default_host();
+        let device = match name {
+            Some(name) => host
+                .input_devices()?
+                .filter_map(|d| d.name().ok().filter(|n| n == name).map(|_| d))
+                .next(),
+            None => host.default_input_device(),
+        }
+        .ok_or(AudioError::NoDevice)?;
+
+        let config = device.default_input_config()?;
+        let stream = device.build_input_stream(
+            &config.config(),
+            move |d: &[f32], _i| {
+                let mut b = buf_s.write().unwrap();
+                for v in d {
+                    b.push(*v);
+                }
+            },
+            |e| {
+                eprintln!("Error occured in the input stream: {e}");
+            },
+            None,
+        )?;
+
+        stream.play()?;
+
+        Ok(Self {
+            buf,
+            stream,
+            device,
+            host,
+        })
+    }
+
+    pub fn available_inputs(&self) -> Result<Vec<String>, AudioError> {
+        Ok(self
+            .host
+            .input_devices()?
+            .filter(|d| d.default_input_config().is_ok())
+            .filter_map(|d| d.name().ok())
+            .collect::<Vec<String>>())
+    }
+
+    pub fn current_name(&self) -> Result<String, AudioError> {
+        Ok(self.device.name()?)
+    }
+
+    pub fn rms(&self) -> f32 {
+        self.buf.read().unwrap().rms()
+    }
+}
 
 pub struct AudioBuffer {
     samples: [f32; 4096],
@@ -37,35 +126,6 @@ impl AudioBuffer {
     }
 }
 
-pub fn create_stream() -> (Stream, Arc<RwLock<AudioBuffer>>) {
-    let buf = Arc::new(RwLock::new(AudioBuffer::default()));
-    let buf_s = buf.clone();
-
-    let host = cpal::default_host();
-    let device = host.default_input_device().unwrap();
-    for c in host.input_devices().unwrap() {
-        println!("{}", c.name().unwrap());
-    }
-    let config = device.default_input_config().unwrap();
-    let stream = device
-        .build_input_stream(
-            &config.config(),
-            move |d: &[f32], _i| {
-                let mut b = buf_s.write().unwrap();
-                for v in d {
-                    b.push(*v);
-                }
-            },
-            |e| {
-                eprintln!("Error occured in the input stream: {e}");
-            },
-            None,
-        )
-        .unwrap();
-
-    (stream, buf)
-}
-
 pub fn gain_to_db(gain: f32) -> f32 {
-    return gain.abs().max(GAIN_MINUS_INFINITY).log10() * 20.0;
+    gain.abs().max(GAIN_MINUS_INFINITY).log10() * 20.0
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,38 @@
-use std::{
-    ffi::{c_uint, CString},
-    sync::{Arc, RwLock},
-};
+use std::ffi::{c_uint, CStr, CString};
 
-use audio::{create_stream, gain_to_db, AudioBuffer};
-use cpal::{
-    traits::{DeviceTrait as _, HostTrait as _, StreamTrait as _},
-    Device, Host, Stream,
-};
+use anyhow::Context as _;
+use audio::{gain_to_db, AudioInterface};
 use raylib::prelude::*;
 use ui::volume_meter;
 
 mod audio;
 mod ui;
 
+enum View {
+    Main,
+    Overlay,
+    Settings {
+        audio_dropdown: bool
+    },
+}
+
 #[allow(unused)]
 struct Application {
+    mouth_closed: Texture2D,
+    mouth_open: Texture2D,
     rl: RaylibHandle,
     thread: RaylibThread,
-    buf: Arc<RwLock<AudioBuffer>>,
-    stream: Stream,
-    mouth_open: Texture2D,
-    mouth_closed: Texture2D,
+    device_list: Vec<String>,
+    current_device: usize,
+    device_text: CString,
+    audio: AudioInterface,
     decorated: bool,
     talking: bool,
     time_release: f32,
     width: f32,
     height: f32,
-    host: Host,
-    devices: Vec<Device>,
-    device_text: CString,
+    view: View,
+    error_message: Option<String>,
 }
 
 impl Application {
@@ -37,8 +40,6 @@ impl Application {
         let (mut rl, thread) = raylib::init().transparent().title("PNG-rs").build();
 
         rl.set_target_fps(60);
-
-        let (stream, buf) = create_stream();
 
         let mouth_open = rl
             .load_texture(&thread, "static/open_mouth.png")
@@ -50,22 +51,20 @@ impl Application {
         let width = rl.get_screen_width() as f32;
         let height = rl.get_screen_height() as f32;
 
-        let host = cpal::default_host();
-        let devices = host.input_devices()?.collect::<Vec<_>>();
-        let mut device_text = devices
-            .iter()
-            .map(|d| d.name().unwrap_or_else(|_| "Unknown device".into()))
-            .collect::<Vec<_>>()
-            .join(";")
-            .into_bytes();
+        let audio = AudioInterface::new(None)?;
+
+        let device_list = audio.available_inputs()?;
+        let mut device_text = device_list.join(";").into_bytes();
         device_text.push(0);
         let device_text = CString::from_vec_with_nul(device_text).unwrap();
+        let current_name = audio.current_name()?;
+        let current_device = device_list.iter().position(|s| *s == current_name)
+            .context("Something really wrong happened when searching for default device index")?;
 
         Ok(Self {
             rl,
             thread,
-            buf,
-            stream,
+            audio,
             mouth_open,
             mouth_closed,
             decorated: true,
@@ -73,14 +72,15 @@ impl Application {
             time_release: 0.0,
             width,
             height,
-            host,
-            devices,
+            device_list,
+            current_device,
             device_text,
+            error_message: None,
+            view: View::Main,
         })
     }
 
     fn run(&mut self) {
-        self.stream.play().unwrap();
         while !self.rl.window_should_close() {
             self.width = self.rl.get_screen_width() as f32;
             self.height = self.rl.get_screen_height() as f32;
@@ -100,15 +100,17 @@ impl Application {
             unsafe {
                 if self.decorated {
                     ffi::SetWindowState(flags);
+                    self.view = View::Overlay;
                 } else {
                     ffi::ClearWindowState(flags);
+                    self.view = View::Main;
                 }
             }
 
             self.decorated = !self.decorated;
         }
 
-        let volume = gain_to_db(self.buf.read().unwrap().rms());
+        let volume = gain_to_db(self.audio.rms());
         if volume > -40.0 {
             self.time_release = 0.25;
         }
@@ -125,46 +127,114 @@ impl Application {
         let mut d = rl.begin_drawing(thread);
 
         let clear_color = match self.decorated {
-            true => Color::new(40, 40, 40, 200),
+            true => Color::new(40, 40, 40, 50),
             false => Color::BLANK,
         };
         d.clear_background(clear_color);
 
-        let bounds = Rectangle {
-            x: 20.0,
-            y: 20.0,
-            width: 20.0,
-            height: self.height as f32 - 70.0,
-        };
-        volume_meter(&mut d, bounds, self.buf.read().unwrap().rms());
+        match self.view {
+            View::Main => {
+                let image_pos = Vector2::new(100.0, 20.0);
+                let image = match self.talking {
+                    true => &self.mouth_open,
+                    false => &self.mouth_closed,
+                };
+                d.draw_texture_ex(image, image_pos, 0.0, 0.5, Color::WHITE);
 
-        d.draw_text(
-            &format!("Talking: {}, time: {}", self.talking, self.time_release),
-            20,
-            self.height as i32 - 30,
-            18,
-            Color::WHITE,
-        );
-        let image_pos = Vector2::new(100.0, 20.0);
-        let image = match self.talking {
-            true => &self.mouth_open,
-            false => &self.mouth_closed,
-        };
-        d.draw_texture_ex(image, image_pos, 0.0, 0.5, Color::WHITE);
+                if icon_button(&mut d, (20.0, 20.0), c"#142#") {
+                    self.view = View::Settings { audio_dropdown: false };
+                }
 
-        let bounds = Rectangle {
-            x: 400.0,
-            y: 20.0,
-            width: 100.0,
-            height: 30.0,
-        };
-        let mut active = 0;
-        d.gui_dropdown_box(bounds, Some(self.device_text.as_c_str()), &mut active, true);
+                let bounds = Rectangle {
+                    x: 20.0,
+                    y: 50.0,
+                    width: 20.0,
+                    height: self.height - 100.0,
+                };
+                volume_meter(&mut d, bounds, self.audio.rms());
+
+                d.draw_text(
+                    &format!("Talking: {}, time: {}", self.talking, self.time_release),
+                    20,
+                    self.height as i32 - 30,
+                    18,
+                    Color::WHITE,
+                );
+            },
+            View::Overlay => {
+                let image_pos = Vector2::new(100.0, 20.0);
+                let image = match self.talking {
+                    true => &self.mouth_open,
+                    false => &self.mouth_closed,
+                };
+                d.draw_texture_ex(image, image_pos, 0.0, 0.5, Color::WHITE);
+            }
+            View::Settings { ref mut audio_dropdown } => 'settings: {
+                if icon_button(&mut d, (20.0, 20.0), c"#72#") {
+                    self.view = View::Main;
+                    break 'settings;
+                }
+
+                let bounds = Rectangle {
+                    x: 20.0,
+                    y: 50.0,
+                    width: 200.0,
+                    height: 30.0,
+                };
+
+                let mut active = self.current_device as i32;
+                if d.gui_dropdown_box(bounds, Some(self.device_text.as_c_str()), &mut active, *audio_dropdown) {
+                    *audio_dropdown = !*audio_dropdown;
+                }
+                if active != self.current_device as i32 {
+                    match AudioInterface::new(Some(&self.device_list[active as usize])) {
+                        Ok(new_audio) => {
+                            self.audio = new_audio;
+                            self.current_device = active as usize;
+                        },
+                        Err(e) => self.error_message = Some(e.to_string()),
+                    }
+                }
+            }
+        }
+
+        if let Some(e) = self.error_message.as_ref() {
+            let mut msg = textwrap::wrap(e, 100).join("\n");
+            msg.push('\0');
+            let w = 500.0;
+            let h = 130.0;
+            let res = d.gui_message_box(
+                Rectangle {
+                    x: (self.width - w) * 0.5,
+                    y: (self.height - h) * 0.5,
+                    width: w,
+                    height: h,
+                },
+                Some(c"Error"),
+                Some(CStr::from_bytes_with_nul(&msg.as_bytes()).unwrap()),
+                Some(c"OK"),
+            );
+
+            if res >= 0 {
+                self.error_message = None;
+            }
+        }
 
         if self.decorated {
             d.draw_rectangle_lines(0, 0, self.width as i32, self.height as i32, Color::BLACK);
         }
     }
+}
+
+fn icon_button(d: &mut RaylibDrawHandle, pos: impl Into<Vector2>, icon: &CStr) -> bool {
+    let pos: Vector2 = pos.into();
+    let bounds = Rectangle {
+        x: pos.x,
+        y: pos.y,
+        width: 20.0,
+        height: 20.0,
+    };
+    d.gui_button(bounds, Some(icon))
 }
 
 fn main() {
