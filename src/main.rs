4a0107dd424fbe1291af27eefc30b18b9fab50a0
Albert Shefner <solaris945@gmail.com>
2025-01-23 21:12:35 +0300

    Switch from Raylib to Egui

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,106 @@
-use std::ffi::{c_uint, CStr, CString};
-
-use anyhow::Context as _;
-use audio::{gain_to_db, AudioInterface};
-use raylib::prelude::*;
-use ui::volume_meter;
+use audio::{gain_to_db, AudioError, AudioInterface};
+use eframe::CreationContext;
+use egui::{
+    include_image, Color32, ComboBox, FontId, Frame, Image, Pos2, Rect, RichText, Vec2,
+    ViewportCommand, ViewportInfo, WindowLevel,
+};
+use ui::AudioUiExt as _;
 
 mod audio;
 mod ui;
 
+fn main() -> eframe::Result {
+    eframe::run_native(
+        "PNGTuber-rs",
+        eframe::NativeOptions {
+            viewport: egui::ViewportBuilder::default().with_transparent(true),
+            ..Default::default()
+        },
+        Box::new(|cc| {
+            let app = Application::new(cc)?;
+            Ok(Box::new(app))
+        }),
+    )
+}
+
 enum View {
     Main,
     Overlay,
-    Settings {
-        audio_dropdown: bool
-    },
+    Settings,
+}
+
+#[derive(Debug, thiserror::Error)]
+enum AppCreationError {
+    #[error("Audio error: {0}")]
+    Audio(#[from] AudioError),
 }
 
 #[allow(unused)]
 struct Application {
-    mouth_closed: Texture2D,
-    mouth_open: Texture2D,
-    rl: RaylibHandle,
-    thread: RaylibThread,
     device_list: Vec<String>,
     current_device: usize,
-    device_text: CString,
     audio: AudioInterface,
-    decorated: bool,
     talking: bool,
     time_release: f32,
-    width: f32,
-    height: f32,
     view: View,
     error_message: Option<String>,
+    top_padding: f32,
 }
 
 impl Application {
-    pub fn new() -> anyhow::Result<Self> {
-        let (mut rl, thread) = raylib::init().transparent().title("PNG-rs").build();
-
-        rl.set_target_fps(60);
-
-        let mouth_open = rl
-            .load_texture(&thread, "static/open_mouth.png")
-            .map_err(|e| anyhow::anyhow!(e))?;
-        let mouth_closed = rl
-            .load_texture(&thread, "static/close_mouth.png")
-            .map_err(|e| anyhow::anyhow!(e))?;
-
-        let width = rl.get_screen_width() as f32;
-        let height = rl.get_screen_height() as f32;
+    pub fn new(cc: &CreationContext) -> Result<Self, AppCreationError> {
+        egui_extras::install_image_loaders(&cc.egui_ctx);
 
         let audio = AudioInterface::new(None)?;
 
         let device_list = audio.available_inputs()?;
         let mut device_text = device_list.join(";").into_bytes();
         device_text.push(0);
-        let device_text = CString::from_vec_with_nul(device_text).unwrap();
         let current_name = audio.current_name()?;
-        let current_device = device_list.iter().position(|s| *s == current_name)
-            .context("Something really wrong happened when searching for default device index")?;
+        let current_device = device_list
+            .iter()
+            .position(|s| *s == current_name)
+            .expect("Something really wrong happened when searching for default device index");
 
         Ok(Self {
-            rl,
-            thread,
             audio,
-            mouth_open,
-            mouth_closed,
-            decorated: true,
             talking: false,
             time_release: 0.0,
-            width,
-            height,
             device_list,
             current_device,
-            device_text,
             error_message: None,
             view: View::Main,
+            top_padding: 0.0,
         })
     }
+}
 
-    fn run(&mut self) {
-        while !self.rl.window_should_close() {
-            self.width = self.rl.get_screen_width() as f32;
-            self.height = self.rl.get_screen_height() as f32;
+impl eframe::App for Application {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let mut dt = 0.0;
+        ctx.input(|i| {
+            dt = i.stable_dt;
 
-            self.update();
-        }
-    }
-
-    fn update(&mut self) {
-        let (rl, thread) = (&mut self.rl, &mut self.thread);
-        let dt = rl.get_frame_time();
-
-        if rl.is_key_pressed(KeyboardKey::KEY_F5) {
-            let flags = ConfigFlags::FLAG_WINDOW_MOUSE_PASSTHROUGH as c_uint
-                | ConfigFlags::FLAG_WINDOW_TOPMOST as c_uint
-                | ConfigFlags::FLAG_WINDOW_UNDECORATED as c_uint;
-            unsafe {
-                if self.decorated {
-                    ffi::SetWindowState(flags);
-                    self.view = View::Overlay;
-                } else {
-                    ffi::ClearWindowState(flags);
-                    self.view = View::Main;
+            if i.key_pressed(egui::Key::F5) {
+                self.view = match self.view {
+                    View::Overlay => View::Main,
+                    _ => View::Overlay,
                 }
             }
 
-            self.decorated = !self.decorated;
-        }
+            if self.top_padding == 0.0 {
+                self.top_padding = get_top_padding(i.viewport()).unwrap_or_default();
+            }
+        });
+
+        let overlay = matches!(self.view, View::Overlay);
+
+        ctx.send_viewport_cmd(ViewportCommand::Decorations(!overlay));
+        ctx.send_viewport_cmd(ViewportCommand::WindowLevel(match overlay {
+            true => WindowLevel::AlwaysOnTop,
+            false => WindowLevel::Normal,
+        }));
+        ctx.send_viewport_cmd(ViewportCommand::MousePassthrough(overlay));
 
         let volume = gain_to_db(self.audio.rms());
         if volume > -40.0 {
@@ -124,120 +116,113 @@ impl Application {
             }
         }
 
-        let mut d = rl.begin_drawing(thread);
+        let mouth_open = Image::new(include_image!("../static/open_mouth.png"));
+        let mouth_closed = Image::new(include_image!("../static/close_mouth.png"));
 
-        let clear_color = match self.decorated {
-            true => Color::new(40, 40, 40, 50),
-            false => Color::BLANK,
-        };
-        d.clear_background(clear_color);
-
-        match self.view {
-            View::Main => {
-                let image_pos = Vector2::new(100.0, 20.0);
-                let image = match self.talking {
-                    true => &self.mouth_open,
-                    false => &self.mouth_closed,
-                };
-                d.draw_texture_ex(image, image_pos, 0.0, 0.5, Color::WHITE);
-
-                if icon_button(&mut d, (20.0, 20.0), c"#142#") {
-                    self.view = View::Settings { audio_dropdown: false };
-                }
-
-                let bounds = Rectangle {
-                    x: 20.0,
-                    y: 50.0,
-                    width: 20.0,
-                    height: self.height - 100.0,
-                };
-                volume_meter(&mut d, bounds, self.audio.rms());
-
-                d.draw_text(
-                    &format!("Talking: {}, time: {}", self.talking, self.time_release),
-                    20,
-                    self.height as i32 - 30,
-                    18,
-                    Color::WHITE,
-                );
-            },
-            View::Overlay => {
-                let image_pos = Vector2::new(100.0, 20.0);
-                let image = match self.talking {
-                    true => &self.mouth_open,
-                    false => &self.mouth_closed,
-                };
-                d.draw_texture_ex(image, image_pos, 0.0, 0.5, Color::WHITE);
-            }
-            View::Settings { ref mut audio_dropdown } => 'settings: {
-                if icon_button(&mut d, (20.0, 20.0), c"#72#") {
-                    self.view = View::Main;
-                    break 'settings;
-                }
-
-                let bounds = Rectangle {
-                    x: 20.0,
-                    y: 50.0,
-                    width: 200.0,
-                    height: 30.0,
-                };
-
-                let mut active = self.current_device as i32;
-                if d.gui_dropdown_box(bounds, Some(self.device_text.as_c_str()), &mut active, *audio_dropdown) {
-                    *audio_dropdown = !*audio_dropdown;
-                }
-                if active != self.current_device as i32 {
-                    match AudioInterface::new(Some(&self.device_list[active as usize])) {
-                        Ok(new_audio) => {
-                            self.audio = new_audio;
-                            self.current_device = active as usize;
+        egui::CentralPanel::default()
+            .frame(
+                Frame::default()
+                    .fill(Color32::TRANSPARENT)
+                    .inner_margin(match overlay {
+                        true => egui::Margin {
+                            top: self.top_padding,
+                            left: 1.0,
+                            ..Default::default()
                         },
-                        Err(e) => self.error_message = Some(e.to_string()),
+                        false => egui::Margin::default(),
+                    }),
+            )
+            .show(ctx, |ui| {
+                let draw_image = |ui| {
+                    let image = match self.talking {
+                        true => &mouth_open,
+                        false => &mouth_closed,
+                    };
+                    let mut pos = Pos2::new(100.0, 20.0);
+                    if matches!(self.view, View::Overlay) {
+                        pos.x += 1.0;
+                        pos.y += self.top_padding;
+                    }
+                    let size = Vec2::new(300.0, 300.0);
+
+                    image.paint_at(ui, Rect::from_min_size(pos, size));
+                };
+
+                match self.view {
+                    View::Main => {
+                        draw_image(ui);
+
+                        if ui.button("S").clicked() {
+                            self.view = View::Settings;
+                        }
+
+                        ui.volume_meter(Vec2::new(50.0, 300.0), self.audio.rms());
+
+                        ui.label(format!(
+                            "Talking: {}, time: {}",
+                            self.talking, self.time_release
+                        ));
+                    }
+                    View::Overlay => draw_image(ui),
+                    View::Settings => 'settings: {
+                        if ui.button("b").clicked() {
+                            self.view = View::Main;
+                            break 'settings;
+                        }
+
+                        let mut current = self.current_device;
+                        ComboBox::from_label("Select audio source")
+                            .selected_text(&self.device_list[self.current_device])
+                            .show_ui(ui, |ui| {
+                                for (i, val) in self.device_list.iter().enumerate() {
+                                    ui.selectable_value(&mut current, i, val);
+                                }
+                            });
+
+                        if current != self.current_device {
+                            match AudioInterface::new(Some(&self.device_list[current])) {
+                                Ok(new_audio) => {
+                                    self.audio = new_audio;
+                                    self.current_device = current;
+                                }
+                                Err(e) => self.error_message = Some(e.to_string()),
+                            }
+                        }
                     }
                 }
-            }
-        }
 
-        if let Some(e) = self.error_message.as_ref() {
-            let mut msg = textwrap::wrap(e, 100).join("\n");
-            msg.push('\0');
-            let w = 500.0;
-            let h = 130.0;
-            let res = d.gui_message_box(
-                Rectangle {
-                    x: (self.width - w) * 0.5,
-                    y: (self.height - h) * 0.5,
-                    width: w,
-                    height: h,
-                },
-                Some(c"Error"),
-                Some(CStr::from_bytes_with_nul(&msg.as_bytes()).unwrap()),
-                Some(c"OK"),
-            );
+                let id = egui::Id::new("Error modal");
+                let modal = egui::Modal::new(id);
+                if let Some(e) = self.error_message.as_ref() {
+                    let mut close = false;
+                    let resp = modal.show(ctx, |ui| {
+                        ui.label(RichText::new("Error").font(FontId::proportional(30.0)));
+                        ui.add_space(5.0);
+                        ui.vertical_centered_justified(|ui| ui.label(e));
+                        ui.add_space(5.0);
+                        ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                            if ui.button("Ok").clicked() {
+                                close = true;
+                            }
+                        })
+                    });
+                    if close || resp.should_close() {
+                        self.error_message = None;
+                    }
+                }
+            });
+    }
 
-            if res >= 0 {
-                self.error_message = None;
-            }
-        }
-
-        if self.decorated {
-            d.draw_rectangle_lines(0, 0, self.width as i32, self.height as i32, Color::BLACK);
+    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
+        match self.view {
+            View::Overlay => [0.0, 0.0, 0.0, 0.0],
+            _ => Color32::from_rgba_unmultiplied(12, 12, 12, 50).to_normalized_gamma_f32(),
         }
     }
 }
 
-fn icon_button(d: &mut RaylibDrawHandle, pos: impl Into<Vector2>, icon: &CStr) -> bool {
-    let pos: Vector2 = pos.into();
-    let bounds = Rectangle {
-        x: pos.x,
-        y: pos.y,
-        width: 20.0,
-        height: 20.0,
-    };
-    d.gui_button(bounds, Some(icon))
-}
-
-fn main() {
-    let mut app = Application::new().expect("Error creating application");
-    app.run();
+fn get_top_padding(viewport: &ViewportInfo) -> Option<f32> {
+    let inner = viewport.inner_rect?.top();
+    let outer = viewport.outer_rect?.top();
+    Some(inner - outer)
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,19 +1,31 @@
-use raylib::prelude::*;
+use egui::{Color32, Pos2, Rect, Rounding, Ui, Vec2};
 
 use crate::audio::gain_to_db;
 
-pub fn volume_meter(d: &mut RaylibDrawHandle, bounds: Rectangle, gain: f32) {
-    let volume = gain_to_db(gain);
-    let value = (60.0 + volume).max(0.0) / 60.0;
-    let h = bounds.height * value;
-    d.draw_rectangle_rec(
-        Rectangle {
-            x: bounds.x,
-            y: bounds.y + bounds.height - h,
-            width: bounds.width,
-            height: h,
-        },
-        Color::GREEN,
-    );
-    d.draw_rectangle_lines_ex(bounds, 1.0, Color::BLACK);
+pub trait AudioUiExt {
+    fn volume_meter(&mut self, size: Vec2, gain: f32);
 }
+
+impl AudioUiExt for Ui {
+    fn volume_meter(&mut self, size: Vec2, gain: f32) {
+        let volume = gain_to_db(gain);
+        let value = (60.0 + volume).max(0.0) / 60.0;
+
+        egui::Frame::default()
+            .stroke(egui::Stroke::new(2.0, Color32::WHITE))
+            .show(self, |ui| {
+                ui.set_min_size(size);
+                let painter = ui.painter();
+                let pos = ui.cursor().min;
+
+                let h = size.y * value;
+                painter.rect_filled(
+                    Rect::from_min_size(Pos2::new(pos.x, pos.y + size.y - h), Vec2::new(size.x, h)),
+                    Rounding::ZERO,
+                    Color32::GREEN,
+                );
+            });
+
+    }
+}
+
